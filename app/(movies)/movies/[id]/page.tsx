c72fe4d29460e8008b9e62fcb35d06f27f67bb97
박문현 <qkransgus1209@gmail.com>
2025-04-08 12:18:00 +0900

    제발돼라

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,24 +1,24 @@
 import { Suspense } from "react";
-import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 
 interface IParams {
   params: { id: string };
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata({ params: { id }}: IParams) {
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
-export default async function MovieDetailPage({ params: { id },
-}: IParams) {
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
+
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
       </Suspense>
